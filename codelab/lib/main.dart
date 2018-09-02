fefd82104396a3d2da1a52d2f839e4f267013cc0
mitohato <mitohato14@gmail.com>
2018-09-02 22:24:27 +0900

    Add import for parse json

--- codelab/lib/main.dart
+++ codelab/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import 'dart:convert';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
